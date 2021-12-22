12632805a116d552142369e0a4db1ec92c7819d7
codingmohamed <cloudchronicles92@gmail.com>
2021-12-22 15:33:26 +0000

    basic test done

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 /* 
 👉 Write your kata here!
+
+Multiple of 4 and 7.
+
+if any number is input, it will check all of the whole integers smaller than the number given and check
+if the number is a multiple of 4 or 7 and add all of them.
+
+If number is both a multiple of 4 and 7, only add it once
+
+Negative numbers should return 0.
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+
+function solution(number) {}
--- main.test.js
+++ main.test.js
@@ -1,1 +1,24 @@
 //👉 Write your tests below here:
+
+function solution(number) {
+  let sum = 0;
+  if (number <= 0) return 0;
+  for (let currNum = 0; currNum < number; currNum++) {
+    if (currNum % 4 === 0 || currNum % 7 === 0) sum += currNum;
+  }
+  return sum;
+}
+
+describe("score of number", () => {
+  const numbers = [
+    [10, 19],
+    [5, 4],
+    [-2565, 0],
+    [-852, 0],
+    [40, 257],
+    [589, 62034],
+  ];
+  it.each(numbers)("should score %i as %i", (number, score) => {
+    expect(solution(number)).toBe(score);
+  });
+});
